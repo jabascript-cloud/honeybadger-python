192efecbbd91284ada855337385ca4006bf70e4a
Kelvin <kelvin.adigwu@centricityinsights.com>
2021-06-24 07:26:39 +0100

    Added log handler

--- honeybadger/contrib/__init__.py
+++ honeybadger/contrib/__init__.py
@@ -1,5 +1,11 @@
 from honeybadger.contrib.flask import FlaskHoneybadger
 from honeybadger.contrib.django import DjangoHoneybadgerMiddleware
 from honeybadger.contrib.aws_lambda import AWSLambdaPlugin
+from honeybadger.contrib.logger import HoneybadgerHandler
 
-__all__ = ['FlaskHoneybadger', 'DjangoHoneybadgerMiddleware', 'AWSLambdaPlugin']
+__all__ = [
+    'FlaskHoneybadger',
+    'DjangoHoneybadgerMiddleware',
+    'AWSLambdaPlugin',
+    'HoneybadgerHandler'
+    ]
